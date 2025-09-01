b03d2fa43b3eb62f7204bf783d747ea1ab822df9
peg7211 <136898660+peg7211@users.noreply.github.com>
2025-09-01 16:35:41 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.cc
+++ 백준/Silver/10845. 큐/큐.cc
@@ -3,70 +3,51 @@
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+	ios::sync_with_stdio(0), cin.tie(0);
 
-	int N;
-	string str;
 	queue<int> q;
-
+	string s;
+	int N, x;
 	cin >> N;
 
 	while (N--) {
-		cin >> str;
+		cin >> s;
 
-		if (str == "push") {
-			int x;
+		if (s == "push") {
 			cin >> x;
-
 			q.push(x);
 		}
-
-		else if (str == "pop") {
-			//비어 있으면 -1 출력
-			if (q.empty()) {
-				cout << "-1" << '\n';
+		else if (s == "pop") {
+			if (q.empty() == 1) {
+				cout << -1 << '\n';
 			}
 			else {
-				//큐에서 가장 앞에 있는 정수 출력
 				cout << q.front() << '\n';
-				//큐에서 가장 앞에 있는 정수 빼기
 				q.pop();
 			}
-			
 		}
-
-		else if (str == "size") {
+		else if (s == "size") {
 			cout << q.size() << '\n';
 		}
-
-		else if (str == "empty") {
-			if (q.empty()) {
-				cout << "1" << '\n';
-			}
-			else {
-				cout << "0" << '\n';
-			}
+		else if (s == "empty") {
+			cout << q.empty() << '\n';
 		}
-
-		else if (str == "front") {
-			if (q.empty()) {
-				cout << "-1"<< '\n';
+		else if (s == "front") {
+			if (q.empty() == 1) {
+				cout << -1 << '\n';
 			}
 			else {
 				cout << q.front() << '\n';
 			}
 		}
-
-		else if (str == "back") {
-			if (q.empty()) {
-				cout << "-1" << '\n';
+		else if (s == "back") {
+			if (q.empty() == 1) {
+				cout << -1 << '\n';
 			}
 			else {
 				cout << q.back() << '\n';
 			}
 		}
 	}
-
 	return 0;
 }
