7ea2624569c6321bc09e6eff748a99df0ce0374c
peg7211 <136898660+peg7211@users.noreply.github.com>
2025-09-01 16:04:40 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,48 +1,66 @@
-#include <iostream> 
-#include <stack>
+#include <iostream>
 using namespace std;
 
+int arr[10000];
+int pos = 0;
+
+void push(int x) {
+	arr[pos++] = x;
+}
+
+void pop() {
+	if (pos == 0) {
+		cout << -1 << '\n';
+	}
+	else {
+		cout << arr[pos-1] << '\n';
+		pos--;
+	}
+}
+
+ int size() {
+	 return pos;
+}
+
+bool empty() {
+	return pos == 0;
+}
+
+void top() {
+	if (pos == 0) {
+		cout << -1 << '\n';
+	}
+	else {
+		cout << arr[pos-1] << '\n';
+	}
+}
 
 int main() {
 	ios::sync_with_stdio(0), cin.tie(0);
 
-	stack<int> s;
-
-	string str;
-	int N;
+	int N, x;
 	cin >> N;
 
 	while (N--) {
+		string str;
 		cin >> str;
 
 		if (str == "push") {
-			int x;
 			cin >> x;
 
-			s.push(x);
+			push(x);
 		}
 		else if (str == "pop") {
-			if (s.size() == 0) {
-				cout << "-1" << '\n';
-			}
-			else {
-				cout << s.top() << '\n';
-				s.pop();
-			}
+			pop();
 		}
 		else if (str == "size") {
-			cout << s.size() << '\n';
+			cout << size() << '\n';
 		}
 		else if (str == "empty") {
-			cout << s.empty() << '\n';
+			cout << empty() << '\n';
 		}
 		else if (str == "top") {
-			if (s.size() == 0) {
-				cout << -1 << '\n';
-			}
-			else {
-				cout << s.top() << '\n';
-			}
+			top();
 		}
 	}
 	return 0;
